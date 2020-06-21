f339cf85485e8b3f51652c2895ff2db784d08a65
Aaron Yates <aaronmyates@hotmail.com>
2020-06-21 14:03:27 -0400

    implemented profile and follower structure to user page

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,34 +1,19 @@
 import React from "react";
-import { Col, Card, CardBody, CardTitle, CardSubtitle, CardImg, ListGroup, ListGroupItem, Button, UncontrolledCollapse } from "reactstrap";
+import { Button, Col, Card, CardBody, CardTitle, CardSubtitle, CardImg } from "reactstrap";
 const placeholder = require("../placeholder.svg");
-const menuIcon = require("../open-menu.svg");
 
 function UserCard(){
 	return(
 		<Col sm="2">
-	    		<Card>
-					<div>
-						<CardImg src={placeholder} />
-						<CardTitle>User Name</CardTitle>
-						<CardSubtitle>Actual Name</CardSubtitle>
-						
-						<Button type="button" id="toggler" block>
-							<svg class="bi bi-list" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-							  <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-							</svg>
-						</Button>
-						
-					    <UncontrolledCollapse toggler="#toggler">
-					          <ListGroup>
-							      <ListGroupItem tag="button">Preferences</ListGroupItem>
-							      <ListGroupItem tag="button">Account Settings</ListGroupItem>
-							      <ListGroupItem tag="button">Song Metrics</ListGroupItem>
-							      <ListGroupItem tag="button" color="danger">Sign Out</ListGroupItem>
-							   </ListGroup>
-					    </UncontrolledCollapse>
-					</div>
-				</Card>
-	    	</Col>
+			<Card>
+				<CardImg src={placeholder} />
+				<CardBody>
+					<CardTitle>User Name</CardTitle>
+					<CardSubtitle>First Name</CardSubtitle>
+					<Button>Add Friend</Button>
+				</CardBody>
+			</Card>
+		</Col>
 	);
 }
 
--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Container, TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col, UncontrolledCollapse, CardBody } from 'reactstrap';
 import classnames from 'classnames';
+import SongCard from "./SongCard";
+import ProfileCard from "./ProfileCard";
 import UserCard from "./UserCard";
 
 function UserPage(){
@@ -14,17 +16,18 @@ function UserPage(){
   	
     <div>
     	<Row>
-    		<UserCard />
+    		<ProfileCard />
     	
 
 		    <Col sm="9">
+
 			      <Nav tabs>
 			        <NavItem>
 			          <NavLink
 			            className={classnames({ active: activeTab === '1' })}
 			            onClick={() => { toggle('1'); }}
 			          >
-			            Tab1
+			            Profile
 			          </NavLink>
 			        </NavItem>
 			        <NavItem>
@@ -32,7 +35,7 @@ function UserPage(){
 			            className={classnames({ active: activeTab === '2' })}
 			            onClick={() => { toggle('2'); }}
 			          >
-			            Moar Tabs
+			            Saved Playlists
 			          </NavLink>
 			        </NavItem>
 			        <NavItem>
@@ -40,16 +43,42 @@ function UserPage(){
 			        		className={classnames({ active: activeTab === '3' })}
 			        		onClick={() => { toggle('3'); }}
 			        	>
-			        		Test Tab
+			        		Followers
 			        	</NavLink>
 			        </NavItem>
 			      </Nav>
+
 			      <TabContent activeTab={activeTab}>
 			        <TabPane tabId="1">
 			          <Row>
-			            <Col sm="12">
-			              <h4>Tab 1 Contents</h4>
+			            <Col sm={{ size: 8, offset:2 }}>
+							<h4>About Me</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+							ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+							fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+							mollit anim id est laborum.</p>
+
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+							ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+							fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+							mollit anim id est laborum.</p>
+
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+							ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+							fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+							mollit anim id est laborum.</p>
 			            </Col>
+			          </Row>
+			          <Row>
+			          	<Col sm="12">
+			          		<h4>Recently Played</h4>
+			          	</Col>
+			          	<Col>
+			          		<SongCard />
+			          	</Col>
 			          </Row>
 			        </TabPane>
 			        <TabPane tabId="2">
@@ -72,9 +101,7 @@ function UserPage(){
 			        </TabPane>
 			        <TabPane tabId="3">
 			        	<Row>
-			        		<Col >
-			        			
-			        		</Col>
+			        		<UserCard />
 			        	</Row>
 			        </TabPane>
 			      </TabContent>
